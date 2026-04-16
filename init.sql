485de6f0296037dd4b757f4d841b372be9c971c7
PawlikKing <pawiklord1@gmail.com>
2026-04-16 22:58:40 +0200

    changed sql init

--- init.sql
+++ init.sql
@@ -8,3 +8,17 @@ CREATE TABLE IF NOT EXISTS users (
     name      VARCHAR(100) NOT NULL,
     flower_id INT REFERENCES flowers(id)
 );
+
+INSERT INTO flowers (id, name) VALUES
+(1, 'Róża'),
+(2, 'Tulipan'),
+(3, 'Stokrotka'),
+(4, 'Lilia'),
+(5, 'Słonecznik');
+
+INSERT INTO users (id, name, flower_id) VALUES
+(1, 'Kamil', 1),
+(2, 'Anna', 2),
+(3, 'Piotr', 3),
+(4, 'Maria', 4),
+(5, 'Jan', 5);
