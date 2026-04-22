0a1b4ea28dcc2f8044ce2668157c89242ad90d9b
PawlikKing <pawiklord1@gmail.com>
2026-04-22 13:35:52 +0200

    schema mismatch fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO Flowers (flowerId, flowerName) VALUES
-(1, 'Ziemniak'),
-(2, 'Barszcz Sosnowskiego'),
-(3, 'Mak'),
-(4, 'Pomidor');
+INSERT INTO Flowers (flowerName) VALUES
+('Ziemniak'),
+('Barszcz Sosnowskiego'),
+('Mak'),
+('Pomidor');
 
-INSERT INTO Users (userId, username, flowerId) VALUES
-(1, 'Pawelo', 1),
-(2, 'Ania', 2),
-(3, 'Jakub', 3);
+INSERT INTO Users (username, flowerId) VALUES
+('Pawelo', 1),
+('Ania', 2),
+('Jakub', 3);
